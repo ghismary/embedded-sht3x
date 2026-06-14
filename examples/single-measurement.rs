1870092778bc5a2ea43cf826c8693bb64e8c1852
Ghislain MARY <ghislain@ghislainmary.fr>
2026-06-14 22:09:39 +0200

    feat: Reset sensor at creation
    This ensures the communication with the sensor is ok right at the
    creation, instead of waiting to trigger a measument.

--- examples/single-measurement.rs
+++ examples/single-measurement.rs
@@ -19,7 +19,7 @@ mod linux {
         }
 
         // Create the sensor and configure its repeatability
-        let mut sensor = Sht3x::new(i2c, DEFAULT_I2C_ADDRESS, hal::Delay {});
+        let mut sensor = Sht3x::new(i2c, DEFAULT_I2C_ADDRESS, hal::Delay {})?;
         sensor.repeatability = High;
 
         // Perform a temperature and humidity measurement
@@ -43,7 +43,7 @@ mod linux {
 
 fn main() {
     #[cfg(target_os = "linux")]
-    linux::main();
+    let _ = linux::main();
     #[cfg(not(target_os = "linux"))]
     println!("This example only works on Linux");
 }
